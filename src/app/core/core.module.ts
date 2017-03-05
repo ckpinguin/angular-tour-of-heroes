1b0da18165d53d765882ab6a01ad1a1212e4ce09
Christof Kälin <christof.kaelin@gmail.com>
2017-03-05 21:06:24 +0100

    small corrections

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,5 @@
-import { NgModule, Optional, SkipSelf }      from '@angular/core';
-import { CommonModule }  from '@angular/common';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { LoggerService } from './logger.service';
 // Prevent more than 1 loading of core
--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { platformBrowser } from '@angular/platform-browser';
 import { enableProdMode } from '@angular/core';
 
+import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
 // The app module factory produced by the static offline compiler
 // import { AppModuleNgFactory } from './app/app.module.ngfactory';
 
-// Enable production mode unless running locally
-if (!/localhost/.test(document.location.host))  {
+if (environment.production) {
   enableProdMode();
 }
 // Launch static AOT with the app module factory.
-// platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
 // Launch with dynamic JIT
 platformBrowserDynamic().bootstrapModule(AppModule)
     .then(success => console.log(`Bootstrap success`))
