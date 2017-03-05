d33f2d5524603b32ba357bd279ad12d8ada9e3c4
Christof Kälin <christof.kaelin@gmail.com>
2017-03-05 20:51:45 +0100

    moved to angular-cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { HttpModule }    from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
 
 import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
 
 import { HeroService } from './heroes/shared/hero.service';
 import { HeroListComponent } from './heroes/hero-list.component/hero-list.component';
