d39e920e841d873159756e56bd3377aa7fb4b9ce
Christof Kälin <christof.kaelin@gmail.com>
2017-03-06 22:15:54 +0100

    trying stuff with tape testing framework, not yet working with ng2 Testbed

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -5,7 +5,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
@@ -16,10 +16,9 @@ var AppComponent = (function () {
 AppComponent = __decorate([
     core_1.Component({
         moduleId: module.id,
-        selector: 'heroes-app',
+        selector: 'toh-heroes-app',
         templateUrl: './app.component.html',
-        styleUrls: ['./app.component.css']
+        styleUrls: ['./app.component.styl']
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
-//# sourceMappingURL=app.component.js.map
--- test.ts
+++ test.ts
@@ -1,64 +1,85 @@
-"use strict";
-exports.__esModule = true;
-var tape_1 = require("tape");
-var testing_1 = require("@angular/core/testing");
-var app_component_1 = require("./src/app/app.component");
-var before = tape_1["default"];
-var after = tape_1["default"];
+import test from 'blue-tape';
+import {TestBed, async} from '@angular/core/testing';
+
+import {AppComponent} from './src/app/app.component';
+
+const before = test;
+const after = test;
+
 // beforeEach/afterEach rely on shared state.
 // That's a big anti-pattern for testing.
+
 // It's also silly to run something before and after
 // ever test -- many of your tests won't need it.
+
 // Try this, instead:
-var setup = function () {
-    var fixture = testing_1.TestBed.createComponent(app_component_1.AppComponent);
+const setup = () => {
+    const fixture = TestBed.createComponent(AppComponent);
     // Insert your fixture code here.
     // Make sure you're creating fresh objects each
     // time setup() is called.
     return fixture;
 };
-var teardown = function (fixture) {
+
+const teardown = (fixture) => {
     // Dispose of your fixtures here.
     fixture = {};
 };
-before('before', function (assert) {
-    testing_1.TestBed.configureTestingModule({
+
+before('before', function(assert) {
+    TestBed.configureTestingModule({
         declarations: [
-            app_component_1.AppComponent
-        ]
+            AppComponent
+        ],
     }).compileComponents();
 });
-tape_1["default"]('Create the app', function (assert) {
-    var fixture = setup();
-    var app = fixture.debugElement.componentInstance;
+
+test('Create the app', (assert) => {
+    const fixture = setup();
+
+    const app = fixture.debugElement.componentInstance;
     assert(app).toBeTruthy();
+
     teardown(fixture);
     assert.end();
 });
-tape_1["default"]('Render title in a h1 tag', function (assert) {
-    var fixture = setup();
+
+test('Render title in a h1 tag', (assert) => {
+    const fixture = setup();
+
     fixture.detectChanges();
-    var compiled = fixture.debugElement.nativeElement;
+    const compiled = fixture.debugElement.nativeElement;
     assert(compiled.querySelector('h1').textContent).toContain('app works!');
+
     teardown(fixture);
     assert.end();
 });
-tape_1["default"]('A test with fixtures', function (assert) {
-    var fixture = setup();
+
+test('A test with fixtures', (assert) => {
+    const fixture = setup();
+
     assert.equal(typeof fixture, 'object', 'fixture should return an object');
+
     teardown(fixture);
     assert.end();
 });
-tape_1["default"]('A passing test', function (assert) {
+
+test('A passing test', (assert) => {
+
     assert.pass('This test will pass.');
+
     assert.end();
 });
-tape_1["default"]('Assertions with tape.', function (assert) {
-    var expected = 'something to test';
-    var actual = 'sonething to test';
+
+test('Assertions with tape.', (assert) => {
+    const expected = 'something to test';
+    const actual = 'sonething to test';
+
     assert.equal(actual, expected, 'Given two mismatched values, .equal() should produce a nice bug report');
+
     assert.end();
 });
+
 /**test('Greet World', (assert) => new Promise((resolve) => {
     assert.equal(console.log('World'), 'Hello, World!');
 
@@ -67,7 +88,9 @@ tape_1["default"]('Assertions with tape.', function (assert) {
         resolve();
     }, 10);
 }));**/
-after('after', function (assert) {
+
+after('after', (assert) => {
     assert.pass('Do something after tests here.');
+
     assert.end();
 });
